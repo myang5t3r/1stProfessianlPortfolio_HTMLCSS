eedc9a9302d2314f0b3a04be8742b9c83bd0546c
myang5t3r <matthew.yang@hunterdouglas.com>
2022-08-20 10:02:58 -0600

    Added 3 new projects with discriptions and working links!

--- script.js
+++ script.js
@@ -21,10 +21,16 @@ slides[slideIndex-1].style.display = "flex";
 
 // My code to launch to future projects, for now just launch to a few websites
 const myarray  = ["https://myang5t3r.github.io/PasswordGenerator/", 
-"https://myang5t3r.github.io/TimedCodeQuiz/", "https://myang5t3r.github.io/myDayScheduler/" ];
+    "https://myang5t3r.github.io/TimedCodeQuiz/", 
+    "https://myang5t3r.github.io/myDayScheduler/",
+    "https://myang5t3r.github.io/myWeatherDashboard/",
+    "https://chuckfee23.github.io/project1-group5/"];
  
 const gitrepo = ["https://github.com/myang5t3r/Professional_Portfolio",
-    "https://github.com/myang5t3r/TimedCodeQuiz", "https://github.com/myang5t3r/myDayScheduler"]
+    "https://github.com/myang5t3r/TimedCodeQuiz", 
+    "https://github.com/myang5t3r/myDayScheduler",
+    "https://github.com/myang5t3r/myWeatherDashboard",
+    "https://github.com/chuckfee23/project1-group5"]
 
 let launchSite = () => {
     window.open(myarray[slideIndex-1])
